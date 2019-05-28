778cb1680d3abe0885d12993412c437f40c737f0
sags95 <alex.sagel7587@gmail.com>
2019-05-27 22:23:41 -0400

    intitial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button, TopAppBar, TopAppBarRow, TopAppBarSection, TopAppBarNavigationIcon, TopAppBarTitle, Grid, GridCell, Typography} from 'rmwc';
+import '@material/button/dist/mdc.button.css';
+import '@material/layout-grid/dist/mdc.layout-grid.css';
+import '@material/top-app-bar/dist/mdc.top-app-bar.css';
+import '@material/typography/dist/mdc.typography.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TopAppBar>
+        <TopAppBarRow>
+          <TopAppBarSection>
+            <TopAppBarNavigationIcon icon="face" />
+            <TopAppBarTitle>My Portfolio</TopAppBarTitle>
+          </TopAppBarSection>
+          <TopAppBarSection alignEnd>
+            <Button
+              label="My work"
+              icon="work"
+              unelevated
+              theme={['secondaryBg', 'onSecondary']}
+            />
+             <Button
+              label="About me"
+              icon="face"
+              unelevated
+              theme={['secondaryBg', 'onSecondary']}
+            />
+          </TopAppBarSection>
+        </TopAppBarRow>
+      </TopAppBar>
+    <div className="landing">
+      <div className="landing_header">
+        <Typography use="headline1">Welcome to my Portfolio</Typography>
+      </div>
     </div>
+   </div>
   );
 }
 
